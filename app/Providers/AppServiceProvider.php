72ecef4c732dd70968539a756fe781148bf7a127
Mohammed Zaid Ali Khan <mzaidx07@gmail.com>
2026-07-28 17:34:38 +0400

    fix: preserve firm context in livewire updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Contracts\MalwareScanner;
+use App\Http\Middleware\ResolveFirmContext;
 use App\Support\FeatureFlags;
 use App\Support\OperationalHealth;
 use App\Support\UnavailableMalwareScanner;
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -37,6 +39,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Livewire::addPersistentMiddleware([
+            ResolveFirmContext::class,
+        ]);
+
         $this->configureDefaults();
     }
 
